2917547fa7321e91fa49d7d3f6a0be69ce4e16c0
daiki0730 <daikibassman@gmail.com>
2022-04-04 20:19:18 +0900

    sampleコントローラを作成

---
+++ app/Http/Controllers/Sample/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Sample;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * @return string
+     */
+    public function show()
+    {
+        return 'Hello';
+    }
+
+    /**
+     * @param $id
+     * @return string
+     */
+    public function showId($id)
+    
+        return "Hello {$id}";
+    }
+}
