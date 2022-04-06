6a1db5e7a75ac07fd2b651f86ea4b5aba64b170a
daiki0730 <daikibassman@gmail.com>
2022-04-06 22:57:04 +0900

    createアクションを作成

---
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Tweet\CreateRequest;
+
+class CreateController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param CreateRequest $request
+     */
+    public function __invoke(CreateRequest $request)
+    {
+        //
+    }
+}
