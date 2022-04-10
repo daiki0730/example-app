4e450222e378af8a92548617eee8d2938cad4ede
daiki0730 <daikibassman@gmail.com>
2022-04-10 23:26:52 +0900

    Update IndexController.php

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -9,7 +9,8 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     /**
-     * @param Request $request
+     * つぶやき一覧
+     ** @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function __invoke(Request $request)
