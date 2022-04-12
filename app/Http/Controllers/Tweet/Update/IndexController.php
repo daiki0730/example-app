9578dc1ee03372a8cd6745863587b04f747a4ba9
daiki0730 <daikibassman@gmail.com>
2022-04-12 23:51:17 +0900

    編集画面を追加

--- app/Http/Controllers/Tweet/Update/IndexController.php
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -9,15 +9,13 @@ use App\Models\Tweet;
 class IndexController extends Controller
 {
     /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function __invoke(Request $request)
     {
         $tweetId = (int) $request->route('tweetId');
         $tweet = Tweet::where('id', $tweetId)->firstOrFail();
-        dd($tweet);
+        return view('tweet.update')->with('tweet', $tweet);
     }
 }
