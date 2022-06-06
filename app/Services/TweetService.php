2a314a5e4cdc80a871f76bf18858de546198f840
daiki0730 <daikibassman@gmail.com>
2022-06-06 22:52:20 +0900

    画像投稿処理を追加

--- app/Services/TweetService.php
+++ app/Services/TweetService.php
@@ -22,10 +22,28 @@ class TweetService
 
         return $tweet->user_id === $userId;
     }
+
     public function countYesterdayTweets(): int
     {
         return Tweet::whereDate('created_at', '>=', Carbon::yesterday()->toDateTimeString())
             ->whereDate('created_at', '<', Carbon::today()->toDateTimeString())
             ->count();
+    }
+
+    public function saveTweet(int $userId, string $content, array $images)
+    {
+        DB::transaction(function () use ($userId, $content, $images) {
+            $tweet = new Tweet;
+            $tweet->user_id = $userId;
+            $tweet->content = $content;
+            $tweet->save();
+            foreach ($images as $image) {
+                Storage::putFile('public/images', $image);
+                $imageModel = new Image();
+                $imageModel->name = $image->hashName();
+                $imageModel->save();
+                $tweet->images()->attach($imageModel->id);
+            }
+        });
     }
 }
